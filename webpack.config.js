19d3092c60ced9da2c311bebdd95626d949f7450
Chandraprakash C <cyberchandru87@gmail.com>
2026-06-06 22:28:24 +0530

    Files altered for vercel error

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-plugins: [
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-    filename: 'index.html',
-    inject: true,
-  }),
-]
